d039e8367f242b3c293b2caeedeb0cf860d8ee18
Jorge Villeda <jorgevilleda0509@outlook.es>
2024-02-15 22:48:07 -0600

    Se agregaron los cambios del ejemplo en el que estoy guiando

--- maincode.cpp
+++ maincode.cpp
@@ -1,20 +1,58 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
+// Definición de la clase Materia
 class Materia {
-	public: 
-		int codigo;
-		string nombre; 
-		float calificacion; 
+public:
+    string nombre;
+    float calificacion;
 };
 
-class Unidad { 
-public: 
-	Materia materias[4]; 
+// Definición de la clase Unidad
+class Unidad {
+public:
+    string nombre;
+    Materia materias[4];
+
+    // Método para ingresar las calificaciones de las materias
+    void ingresarCalificaciones() {
+        cout << "Ingrese las calificaciones para la unidad " << nombre << endl;
+        for (int i = 0; i < 4; ++i) {
+            cout << "Calificación para la materia " << materias[i].nombre << ": ";
+            cin >> materias[i].calificacion;
+        }
+    }
 };
 
-    return 0; 
+int main() {
+    // Definir las materias predefinidas
+    string nombresMaterias[4] = {"Matemáticas", "Ciencias", "Historia", "Literatura"};
+
+    Unidad unidades[4];
+
+    // Asignar nombres a las unidades
+    for (int i = 0; i < 4; ++i) {
+        unidades[i].nombre = "Unidad " + i + 1;
+        // Asignar nombres a las materias para cada unidad
+        for (int j = 0; j < 4; ++j) {
+            unidades[i].materias[j].nombre = nombresMaterias[j];
+        }
+    }
+
+    // Ingresar calificaciones para cada unidad
+    for (int i = 0; i < 4; ++i) {
+        unidades[i].ingresarCalificaciones();
+    }
+
+    // Mostrar las calificaciones ingresadas para cada unidad y materia
+    for (int i = 0; i < 4; ++i) {
+        cout << "Calificaciones para la Unidad " << i + 1 << ":" << endl;
+        for (int j = 0; j < 4; ++j) {
+            cout << "  " << unidades[i].materias[j].nombre << ": " << unidades[i].materias[j].calificacion << endl;
+        }
+    }
+
+    return 0;
 }
-aa
+
